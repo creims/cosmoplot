c882b5ca40e88909e1b535ce2d687957a2413e15
creims <colin.reims@gmail.com>
2019-08-25 13:04:18 -0600

    Updates

---
+++ precache-manifest.7a43860c525f3283e5fce71ef8f5def0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d9bfe9afa8d5ba7e616a819382dc08",
+    "url": "/cosmoplot/index.html"
+  },
+  {
+    "revision": "294ea6ac3202576fb8bf",
+    "url": "/cosmoplot/static/css/main.29395b28.chunk.css"
+  },
+  {
+    "revision": "148bf0919546a7364fe5",
+    "url": "/cosmoplot/static/js/2.fa5d493e.chunk.js"
+  },
+  {
+    "revision": "294ea6ac3202576fb8bf",
+    "url": "/cosmoplot/static/js/main.f309b128.chunk.js"
+  },
+  {
+    "revision": "6f8c30e9cea5cccfb7f8",
+    "url": "/cosmoplot/static/js/runtime~main.b9547e2b.js"
+  }
+]);
